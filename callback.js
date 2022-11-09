e96847b045062d0c887e73eec0cbb05949abbcbb
tot <TopiGit321>
2022-11-10 01:54:03 +0800

    update

--- callback.js
+++ callback.js
@@ -1,13 +1,72 @@
 // Callback, Promise, Async await
 // yang di atas tujuan sama tapi beda penulisan aja yang berbeda
 // ini digunakan ketika kita sudah mendapatkan sesuatu trus kita mau apain
-
 // contoh
 const users = [
     {username: "rosyid"},
     {username: "budi"},
     {username: "lukman"},
 ]
+
+// ASYNC AWAIT
+// lebih simple dari callback dan promise
+const getUser = () => {
+    setTimeout(() => {
+        console.log(users);
+    }, 1000);
+}
+const createUser = (user) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            users.push(user);
+            let error = false;
+            if(!error){
+                resolve()
+            }else{
+                reject('Ini error cuy')
+            }
+        }, 2000);
+    })
+}
+// async function testing() {
+// }
+const testing = async () => {
+    try {
+        await createUser({username: "budi hartono"});
+        getUser();
+    } catch (error) {
+        console.log(error);
+    }
+}
+testing();
+
+// PROMISE
+// untuk promise itu membawa 2 parameter yaitu resolve dan reject
+// const getUser = () => {
+//     setTimeout(() => {
+//         console.log(users);
+//     }, 1000)
+// }
+// const createUser = (user) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             users.push(user);
+//             let error = false;
+//             if(!error){
+//                 resolve()
+//             }else {
+//                 reject('Shomething went wrong...!')
+//             }
+//         }, 2000);
+//     })
+// }
+// createUser({username: "guli lam"})
+//     .then(getUser)
+//     .catch((msg) => {
+//         console.log(msg);
+// })
+
+
 // yang kita mau ketika kita createdata nya 2 detik kita mau memunculkan datanya
 // penulisan callback
 // function createData(user, callback) {
@@ -22,7 +81,6 @@ const users = [
 //         console.log(users);
 //     }, 1000)
 // })
-
 // PENULISAN callback arrow function
 // const createData = (user, callback) => {
 //     setTimeout(() => {
